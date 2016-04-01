797ef270be3fd1d2909f56f08c670691a91420f3
dirgepye <s.stenhouse87@gmail.com>
2016-04-01 04:28:23 +0000

    splash page added

--- script/script.js
+++ script/script.js
@@ -22,6 +22,19 @@ $(document).ready(function() {
         });
     });
 
+      $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 280) {
+      $('.nav-bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 281) {
+      $('.nav-bar').removeClass('navbar-fixed');
+    }
+  });
+
 
 //MAP  ////////////
 
